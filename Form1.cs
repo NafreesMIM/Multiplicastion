0e0ff447b0f2e1b1ac4468e97e5a67537ae71fb3
NafreesMIM <143109431+NafreesMIM@users.noreply.github.com>
2024-06-22 11:54:20 +0530

    FU

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplication_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox1.Select();
+            int no = Convert.ToInt32(textBox1.Text);
+            for (int i = 1; i <= 20; i++)
+            {
+                listBox1.Items.Add(i+"   "+" X "+" "+no+" "+"="+" "+i*no);
+            }
+        }
+    }
+}
